1b60c08ad3f2e576b63f23431656b71387028522
oluwaseni <oluwasenijbd@gmail.com>
2020-06-02 22:44:51 +0100

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  contact(){
+      this._router.navigate(['app/contact'])
+  }
+  blog(){
+      this._router.navigate(['app/blog'])
+  }
+  home(){
+      this._router.navigate(['app/home'])
   }
 
 }
